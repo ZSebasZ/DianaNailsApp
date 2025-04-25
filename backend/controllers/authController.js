30447db0e2baf1670da2b45d879020e008142b5b
Sebastian Jimenez <sarboledacarballeira@gmail.com>
2025-04-25 21:07:13 +0200

    Commit 14 - Hice el bloque de codigo que crea el carrito de compras al cliente registrado en la funcion que registra un cliente

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,28 +4,28 @@ import connection from "./../db/connection.js"; //Importamos nuestra conexion
 const login = (req, res) => {
 
     //Obtenemos el email y contraseña que el frontend nos envia
-    const {email, contrasena} = req.body;
+    const { email, contrasena } = req.body;
 
     //Si alguno de los datos está vació o no se envia, mandamos un error
     if (!email || !contrasena) {
-        return res.status(400).json({mensaje: "Campos incompletos"})
+        return res.status(400).json({ mensaje: "Campos incompletos" })
     }
 
     //Hacemos la query en la base de datos para saber si el email existe
     const queryUsuario = "SELECT * FROM usuarios WHERE email = ?"
     connection.query(queryUsuario, [email], (error, results) => {
-        
+
         //Si no existe enviamos un mensaje de error
-        if(results.length == 0) {
-            return res.status(404).json({mensaje: "Usuario no encontrado"})
+        if (results.length == 0) {
+            return res.status(404).json({ mensaje: "Usuario no encontrado" })
         }
 
         //De lo contrario obtenemos el usuario
         const usuario = results[0]
 
         //Si la contraseña no es la correcta enviamos un mensaje de aviso
-        if(usuario.contrasena != contrasena) {
-            return res.status(401).json({mensaje: "Contraseña incorrecta"})
+        if (usuario.contrasena != contrasena) {
+            return res.status(401).json({ mensaje: "Contraseña incorrecta" })
         }
 
         //Si todo esta correcta, enviamos un mensaje indicando el inicio correcto de sesion
@@ -41,6 +41,7 @@ const login = (req, res) => {
 }
 
 //Creamos la funcion que se encarga del REGISTRO de USUARIOS-CLIENTES
+//Ademas de crear su correspondiente CARRITO
 const registerCliente = (req, res) => {
 
     //Obtenemos el email y contraseña que el frontend nos envia
@@ -79,8 +80,21 @@ const registerCliente = (req, res) => {
                     return res.status(500).json({ mensaje: "Error al registrar el cliente" });
                 }
 
-                //Si todo el proceso fue exitoso, monstramos un mensaje
-                res.status(201).json({ mensaje: "Cliente registrado con exito. ID usuario: " +  idUsuarioInsertado + " - ID cliente: " + idUsuarioInsertado});
+
+                //Hacemos la insercion del carrito del cliente registrado
+                const insertCarritoCliente = "INSERT INTO carritos (id_cliente, subtotal) VALUES (?, 0)"
+                connection.query(insertCarritoCliente, [idUsuarioInsertado], (error, result) => {
+                    //Si ocurre algun error en la insercion, mostramos un mensaje
+                    if (error) {
+                        return res.status(500).json({ mensaje: "Error al crear el carrito del cliente" });
+                    }
+
+                    const idCarritoInsertado = result.insertId;
+                    
+                    //Si todo el proceso fue exitoso, monstramos un mensaje
+                    res.status(201).json({ mensaje: "Cliente registrado con exito. ID usuario: " + idUsuarioInsertado + " - ID cliente: " + idUsuarioInsertado + " - ID carrito: " + idCarritoInsertado });
+
+                })
             })
         })
     })
@@ -117,7 +131,7 @@ const registerManicurista = (req, res) => {
             //Obtenemos el id del usuario insertado
             const idUsuarioInsertado = result.insertId;
 
-            //Hacemos la insercion del nuevo cliente
+            //Hacemos la insercion de la nueva manicurista
             const insertNuevoCliente = "INSERT INTO manicuristas(id, dni, estrellas) VALUES (?, ?, ?)"
             connection.query(insertNuevoCliente, [idUsuarioInsertado, dni, 5], (error, result) => {
                 //Si ocurre algun error en la insercion, mostramos un mensaje
@@ -126,10 +140,10 @@ const registerManicurista = (req, res) => {
                 }
 
                 //Si todo el proceso fue exitoso, monstramos un mensaje
-                res.status(201).json({ mensaje: "Manicurista registrada con exito. ID usuario: " +  idUsuarioInsertado + " - ID manicurista: " + idUsuarioInsertado});
+                res.status(201).json({ mensaje: "Manicurista registrada con exito. ID usuario: " + idUsuarioInsertado + " - ID manicurista: " + idUsuarioInsertado });
             })
         })
     })
 }
 
-export {login, registerCliente, registerManicurista}
+export { login, registerCliente, registerManicurista }
