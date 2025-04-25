fad4b9218341e4c8629f65c997146be7db35b2f4
Sebastian Jimenez <sarboledacarballeira@gmail.com>
2025-04-25 22:34:41 +0200

    Commit 16 - Hice la ruta y la funcion que actualiza los datos personales de un cliente

--- backend/controllers/perfilController.js
+++ backend/controllers/perfilController.js
@@ -1,12 +1,12 @@
 import connection from "./../db/connection.js"; //Importamos nuestra conexion
 
-//Creamos la funcion que se encarga de la OBTENCION de los datos del USUARIO
+//Creamos la funcion que se encarga de la ACTUALIZACION de los datos personales de una MANICURISTA
 const updateDatosPersManicurista = (req, res) => {
 
     //Obtenemos el id de la manicurista a actualizat
     const { id } = req.params
 
-    //Obtenemos los servicios seleccionados
+    //Obtenemos los datos enviados
     const { url_imagen, nombre, apellidos, telefono } = req.body;
 
     //Si alguno de los datos está vació o no se envia, mandamos un error
@@ -33,4 +33,54 @@ const updateDatosPersManicurista = (req, res) => {
     })
 }
 
-export {updateDatosPersManicurista}
+//Creamos la funcion que se encarga de la ACTUALIZACION de los datos personales de un CLIENTE
+const updateDatosPersCliente = (req, res) => {
+
+    //Obtenemos el id del cliente a actualizat
+    const { id } = req.params
+
+    //Obtenemos los datos enviados
+    const { url_imagen, nombre, apellidos, telefono, direccion_envio } = req.body;
+
+    //Si alguno de los datos está vació o no se envia, mandamos un error
+    if (!nombre || !apellidos || !telefono) {
+        return res.status(400).json({ mensaje: "Campos incompletos" })
+    }
+
+    //Setencia SQL para actualizar los datos personales del cliente
+    const updateDatosPersCliente = "UPDATE usuarios SET url_imagen = ?, nombre = ?, apellidos = ?, telefono = ? WHERE id = ?"
+    //Hacemos la actualizacion 
+    connection.query(updateDatosPersCliente, [url_imagen, nombre, apellidos, telefono, id], (error, result) => {
+        //Si ocurre algun error en la actualizacion, mostramos un mensaje
+        if (error) {
+            return res.status(500).json({ mensaje: "Error al actualizar los datos del cliente" });
+        }
+
+        //Comprobamos si alguna fila se actualizó
+        if (result.affectedRows == 0) {
+            return res.status(404).json({ message: "Cliente no encontrado" });
+        }
+
+        //Setencia SQL para actualizar la direccion de envio del cliente
+        const updateDatosPersCliente = "UPDATE clientes SET direccion_envio = ? WHERE id = ?"
+        //Hacemos la actualizacion 
+        connection.query(updateDatosPersCliente, [direccion_envio, id], (error, result) => {
+            //Si ocurre algun error en la actualizacion, mostramos un mensaje
+            if (error) {
+                return res.status(500).json({ mensaje: "Error al actualizar los datos del cliente" });
+            }
+
+            //Comprobamos si alguna fila se actualizó
+            if (result.affectedRows == 0) {
+                return res.status(404).json({ message: "Cliente no encontrado" });
+            }
+
+            //Si todo el proceso fue exitoso, monstramos un mensaje
+            res.status(200).json({ message: "Cliente actualizado correctamente" });
+        })
+    })
+}
+
+
+
+export { updateDatosPersManicurista, updateDatosPersCliente }
--- backend/routes/perfil.js
+++ backend/routes/perfil.js
@@ -1,5 +1,5 @@
 import express from "express"; //Importamos la libreria 'express'
-import {updateDatosPersManicurista} from "./../controllers/perfilController.js"
+import {updateDatosPersManicurista, updateDatosPersCliente} from "./../controllers/perfilController.js"
 
 //Creamos el objeto Router para establecer las rutas que vamos a usar
 const router = express.Router()
@@ -13,6 +13,19 @@ router.put("/update-datos-pers-manicurista/:id", updateDatosPersManicurista)
         "nombre": "Manicurista actualizada",
         "apellidos": "Actualizada",
         "telefono": "600000000"
+    }
+*/
+
+//Creamos la ruta para la ACTUALIZACION de los datos personales de un CLIENTE
+router.put("/update-datos-pers-cliente/:id", updateDatosPersCliente)
+//JSON DE PRUEBA
+/*
+    {
+        "url_imagen": "",
+        "nombre": "Cliente1",
+        "apellidos": "Actualizado",
+        "telefono": "611111111",
+        "direccion_envio": "direccion actualizada"
     }
 */
 
